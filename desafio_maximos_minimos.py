ffd5db606e62517dabcca18e6956b89450ac57a4
Nelly <burgosn871@gmail.com>
2024-07-13 12:26:29 -0300

    Inicio 2024

---
+++ desafio_maximos_minimos.py
@@ -0,0 +1,23 @@
+""""
+Ahora el siguiente desafío consiste en hacer que el programa que tuvimos de prueba muestre
+tanto el valor máximo ingresado como el minimo.
+Ahora.. si lograste realizarlo utilizando el código de muestra hasta el momento, 
+¿Qué sucede si ingresamos números consecutivos como 1,2,3,4?
+
+"""
+contador=0
+min_numero = 999999     
+max_numero = -999999    
+
+while contador < 4:
+    num = int(input("ingrese un numero:"))
+
+    if num < min_numero:
+        min_numero = num
+   
+    if num > max_numero:
+        max_numero = num
+    contador+=1
+print("Cantidad de numero ingresado: ",contador)
+print("el valor minimo es: ", min_numero)
+print("el valor maximo es: ", max_numero)
