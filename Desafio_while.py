bd4cda80d3c35b55715bcf311671507eb9c99630
Nelly <burgosn871@gmail.com>
2024-07-13 12:25:32 -0300

    Inicio 2024

---
+++ Desafio_while.py
@@ -0,0 +1,10 @@
+""""
+Enunciado:
+Crear un programa que muestre en la consola 
+10 repeticiones con números ASCENDENTE desde el 1 al 10
+"""
+
+contador = 1  
+while contador <= 10:
+    print(contador)
+    contador += 1  
